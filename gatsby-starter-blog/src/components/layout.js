f3b7fdaa1fcb7aaabdff368c772814e96f73fedd
Abhinav Ramachandran <geckods@gmail.com>
2025-01-17 20:16:35 -0800

    Adding basic auth

--- gatsby-starter-blog/src/components/layout.js
+++ gatsby-starter-blog/src/components/layout.js
@@ -1,5 +1,7 @@
 import * as React from "react"
 import { Link } from "gatsby"
+import AuthNavBar from "./AuthNavBar"
+
 
 const Layout = ({ location, title, children }) => {
   const rootPath = `${__PATH_PREFIX__}/`
@@ -21,11 +23,14 @@ const Layout = ({ location, title, children }) => {
   }
 
   return (
-    <div className="global-wrapper" data-is-root-path={isRootPath}>
-      <header className="global-header">{header}</header>
-      <main>{children}</main>
-    </div>
-  )
+    <>
+      <AuthNavBar />
+      <div className="global-wrapper" data-is-root-path={isRootPath}>
+        <header className="global-header">{header}</header>
+        <main>{children}</main>
+      </div>
+    </>
+ )
 }
 
 export default Layout
--- gatsby-starter-blog/src/pages/index.js
+++ gatsby-starter-blog/src/pages/index.js
@@ -1,58 +1,62 @@
 import * as React from "react"
 import { Link, graphql } from "gatsby"
-
 import Bio from "../components/bio"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import NavBar from '../components/NavBar'
+import PrivateRoute from '../components/privateRoute'
+import { getUser, isLoggedIn } from "../services/auth"
 
 const BlogIndex = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata?.title || `Title`
   const posts = data.allMarkdownRemark.nodes
 
-  if (posts.length === 0) {
-    return (
-      <Layout location={location} title={siteTitle}>
-        <NavBar />
-        <p>
-          No blog posts found. Add markdown posts to "content/blog" (or the
-          directory you specified for the "gatsby-source-filesystem" plugin in
-          gatsby-config.js).
-        </p>
-        <Bio className="bio-spacing"/>
-      </Layout>
-    )
-  }
-
   return (
     <Layout location={location} title={siteTitle}>
-      <NavBar />
-      <ol style={{ listStyle: `none` }}>
-        {posts.map(post => {
-          const title = post.frontmatter.title || post.fields.slug
+      <div>
+        {isLoggedIn() ? (
+          <>
+            <NavBar />
+            <ol style={{ listStyle: `none` }}>
+              {posts.map(post => {
+                const title = post.frontmatter.title || post.fields.slug
+                const isPrivate = post.frontmatter.private || false
 
-          return (
-            <li key={post.fields.slug}>
-              <article
-                className="post-list-item"
-                itemScope
-                itemType="http://schema.org/Article"
-              >
-                <header>
-                  <small>{post.frontmatter.date}</small>
-                  <h2>
-                    <Link to={post.fields.slug} itemProp="url">
-                      <span itemProp="headline">{title}</span>
-                    </Link>
-                  </h2>
-                </header>
-              </article>
-            </li>
-          )
-        })}
-      </ol>
-      <div className="bio-spacing">
-        <Bio />
+                const PostComponent = () => (
+                  <li key={post.fields.slug}>
+                    <article
+                      className="post-list-item"
+                      itemScope
+                      itemType="http://schema.org/Article"
+                    >
+                      <header>
+                        <small>{post.frontmatter.date}</small>
+                        <h2>
+                          <Link to={post.fields.slug} itemProp="url">
+                            <span itemProp="headline">{title}</span>
+                          </Link>
+                        </h2>
+                      </header>
+                    </article>
+                  </li>
+                )
+
+                return isPrivate ? (
+                  <PrivateRoute component={PostComponent} key={post.fields.slug} />
+                ) : (
+                  <PostComponent key={post.fields.slug} />
+                )
+              })}
+            </ol>
+            <div className="bio-spacing">
+              <Bio />
+            </div>
+          </>
+        ) : (
+          <>
+            You should <Link to="/app/login">log in</Link> to see restricted content
+          </>
+        )}
       </div>
     </Layout>
   )
@@ -60,11 +64,6 @@ const BlogIndex = ({ data, location }) => {
 
 export default BlogIndex
 
-/**
- * Head export to define metadata for the page
- *
- * See: https://www.gatsbyjs.com/docs/reference/built-in-components/gatsby-head/
- */
 export const Head = () => <Seo title="All posts" />
 
 export const pageQuery = graphql`
@@ -87,6 +86,7 @@ export const pageQuery = graphql`
           date(formatString: "YYYY MMM DD")
           title
           description
+          private
         }
       }
     }
